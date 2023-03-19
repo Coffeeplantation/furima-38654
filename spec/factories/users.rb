2528493803d54b93b50f74554e6f51e7fd7bea27
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2023-03-19 15:13:13 +0900

    パスワード：ダミーデータの追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'test1234TEST'}
     password_confirmation {password}
     last_name             {'太郎'}
     family_name           {'山田'}
