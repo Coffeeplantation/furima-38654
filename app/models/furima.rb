9f7068d750a52fd60743bb06341ccde10e31447d
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2023-03-30 21:59:56 +0900

    出品画面表示

--- app/models/furima.rb
+++ app/models/furima.rb
@@ -1,7 +1,16 @@
 class Furima < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :fee
+  belongs_to :price
   belongs_to :furima
   belongs_to :user
   has_one_attached :image
 
   validates :content, presence: true
+  validates :furima_text, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
 end
