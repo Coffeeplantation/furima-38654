1799d305f2ec380f6915c76d8de0e964dffaaa08
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2023-04-08 20:48:34 +0900

    def move_to_index 削除

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -44,10 +44,6 @@ class FurimasController < ApplicationController
    # @furima = Furima.find(params[:id])
   #end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
 
     private
 
